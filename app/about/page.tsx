d3ed55a85602b36c576b44005d1dc378551e5db0
Joel Bello <joel8bl@gmail.com>
2024-02-29 01:55:39 +0100

    first com

---
+++ app/about/page.tsx
@@ -0,0 +1,66 @@
+import Image from "next/image";
+import Link from "next/link";
+import joelnbl from 'public/images/home/joelnbl.jpg';
+import platzi from 'public/images/home/platzi.jpg';
+import coffee from 'public/images/home/coffee.jpg';
+import learning_platzi from 'public/images/home/learning_platzi.jpg';
+
+function Badge(props) {
+  return (
+    <a
+      {...props}
+      target="_blank"
+      className="border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded p-1 text-sm inline-flex items-center leading-4 text-neutral-900 dark:text-neutral-100 no-underline"
+    />
+  );
+}
+
+export default function Page() {
+  return (
+    <section>
+
+       <div className="columns-1 sm:columns-2 gap-4 my-8">
+        <div className="relative h-80 mb-4">
+          <Image
+            alt="Me speaking on stage at React Summit about the future of Next.js"
+            src={joelnbl}
+            fill
+            sizes="(max-width: 768px) 213px, 33vw"
+            priority
+            className="rounded-lg object-cover"
+          />
+        </div>
+        <div className="relative h-80 mb-4 sm:mb-0">
+          <Image
+            alt="Me, Lydia, and Delba filming the Next.js Conf keynote"
+            src={platzi}
+            fill
+            sizes="(max-width: 768px) 213px, 33vw"
+            priority
+            className="rounded-lg object-cover object-[-16px] sm:object-center"
+          />
+        </div> 
+        <div className="relative h-40 sm:h-80 sm:mb-4">
+          <Image
+            alt="Me standing on stage at Reactathon delivering the keynote"
+            src={coffee}
+            fill
+            sizes="(max-width: 768px) 213px, 33vw"
+            priority
+            className="rounded-lg object-cover object-top sm:object-center"
+          />
+        </div>
+        <div className="relative h-40 mb-4 sm:mb-0">
+          <Image
+            alt="Me standing on stage at SmashingConf giving a talk about my optimism for the web"
+            src={learning_platzi}
+            fill
+            sizes="(max-width: 768px) 213px, 33vw"
+            priority
+            className="rounded-lg object-cover"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ app/layout.tsx
@@ -0,0 +1,59 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Navbar } from "./components/nav";
+import joelnbl from 'public/images/home/joelnbl.jpg';
+import Image from "next/image";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Joel Bello",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className="antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
+        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+          <Navbar />
+          <div className="flex justify-around gap-4">
+
+        <div className="flex justify-center">
+          <Image
+            alt="Me speaking on stage at React Summit about the future of Next.js"
+            src={joelnbl}
+            width={800}
+            height={150}
+            sizes="(max-width: 768px) 213px, 33vw"
+            priority
+            className="rounded-lg object-cover"
+          />
+       </div> 
+      <div>
+      <h1 className="font-medium text-2xl mb-4 text-left tracking-tighter">
+        hey, I'm Joel 👋
+      </h1>
+      <div className="prose prose-neutral dark:prose-invert">
+        <p>
+        Highly motivated and experienced Full-Stack Developer with
+        a passion for building innovative and user-friendly web
+        applications. Proven ability to design, develop, and deploy
+        applications across various technologies. Adept at
+        collaborating with cross-functional teams to deliver
+        successful projects on time and within budget.
+        </p>
+      </div>
+      </div>
+
+          </div>
+        {children}
+        </main>
+      </body>
+    </html>
+  );
+}
