34aab48683a28e07175897180bd5312279427bf1
Joel Bello <joel8bl@gmail.com>
2024-02-29 02:34:38 +0100

    fixing hero responsive

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,8 +21,8 @@ export default function RootLayout({
       <body className="antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           <Navbar />
-          <div className="flex justify-around gap-4">
-        <div className="flex justify-center">
+          <div className="flex flex-col sm:flex-row justify-around gap-4">
+        <div >
           <Image
             alt="Me speaking on stage at React Summit about the future of Next.js"
             src={joelnbl}
