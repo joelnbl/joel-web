0efff7a344e1646849ffcd44ee8640fc994a477a
Joel Bello <joel8bl@gmail.com>
2024-03-05 01:40:40 +0100

    adding logo and content to projects

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
         <main className="antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
           <div className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           <Navbar />
-          <div className="rounded-md p-4 bg-blue-50 dark:bg-blue-950 flex flex-col sm:flex-row justify-around gap-4">
+          <div className="rounded-md p-4 bg-[#068FFF] dark:bg-[#4E4FEB] text-white flex flex-col sm:flex-row justify-around gap-4">
         <div >
           <Image
             alt="Me speaking on stage at React Summit about the future of Next.js"
